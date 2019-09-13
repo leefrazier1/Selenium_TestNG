aa82c1e38f2ea29055551a3c5f5592b025db3955
leefrazier1 <52464909+leefrazier1@users.noreply.github.com>
2019-09-13 11:50:28 -0400

    Update FactoryTest.java

--- src/test/java/FactoryTest.java
+++ src/test/java/FactoryTest.java
@@ -9,9 +9,9 @@ public class FactoryTest {
         Object[] res = new Object[5];
         res[0] = new FactoryImplTestAdd(2,3,5);
         res[1] = new FactoryImplTestAdd(3,3,6);
-        res[2] = new FactoryImplTestAdd(1,3,5);
+        res[2] = new FactoryImplTestAdd(1,3,4);
         res[3] = new FactoryImplTestSubtract(4,2,2);
-        res[4] = new FactoryImplTestSubtract(4,1,2);
+        res[4] = new FactoryImplTestSubtract(4,1,3);
         return res;
     }
 }
